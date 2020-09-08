a963adb550c308d57be4cfd36a1a9360d78f104a
a632079 <i@a632079.me>
2020-09-08 21:37:45 +0800

    fix: lint

--- src/test/sentence/v1/get.test.ts
+++ src/test/sentence/v1/get.test.ts
@@ -1,6 +1,8 @@
-import {getSentence} from '@App/sentence/v1/core'
+import { getSentence } from '@App/sentence/v1/core'
 
 test('测试是否能正常获得一言语句', async () => {
   const sentence = await getSentence()
-  expect(sentence.uuid).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)
+  expect(sentence.uuid).toMatch(
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
+  )
 })
