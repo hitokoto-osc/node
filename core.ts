470692282e1ea0565f118fb083460234803647a0
a632079 <i@a632079.me>
2020-09-08 21:31:22 +0800

    chore: lint core

--- core.ts
+++ core.ts
@@ -5,10 +5,11 @@ import { CoreApi as CoreApiV1 } from './src/core/v1/core'
 export { CoreApiV1 }
 
 export interface SentenceApiV1 {
-  getSentence(categroy?: string | string[] | undefined): Promise<sentenceApiResponse>
+  getSentence(
+    categroy?: string | string[] | undefined,
+  ): Promise<sentenceApiResponse>
 }
 
-export class SentenceApiV1 {
-}
+export class SentenceApiV1 {}
 
 SentenceApiV1.prototype.getSentence = getSentence
